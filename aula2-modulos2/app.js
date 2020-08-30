247a22539837277bee255a3bbfa0f28749805c73
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-08-30 19:56:20 -0300

    aula2 - modulos 2

---
+++ aula2-modulos2/app.js
@@ -0,0 +1,9 @@
+// const multipliacacao = require('./mod1');
+
+// console.log(multipliacacao(2, 2));
+
+// console.log(__filename);
+// console.log(__dirname);
+
+const path = require('path');
+console.log(path.resolve(__dirname));
