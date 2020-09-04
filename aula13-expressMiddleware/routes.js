998eaa2fed223544c11013634f2a8149014f1856
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-09-04 11:02:19 -0300

    aula13 Middlewares

--- aula13-expressMiddleware/routes.js
+++ aula13-expressMiddleware/routes.js
@@ -3,20 +3,8 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-function meuMiddleware(req, res, next) {
-  req.session = { nome: 'Matheus', sobrenome: 'Figueiredo'};
-  console.log();
-  console.log('Passei no seu middleware');
-  console.log();
-  next();
-}
-
 // Rotas da home
-route.get('/', meuMiddleware, homeController.paginaInicial, (req, res, next) => {
-  console.log();
-  console.log('Ainda estou aqui')
-  console.log(`'último middleware' - Olha o que tem na req.session.nome: ${req.session.nome}`)
-});
+route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
 // Rotas de contato
--- aula13-expressMiddleware/server.js
+++ aula13-expressMiddleware/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const meuMiddleware = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -9,6 +10,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(meuMiddleware); // Todas as requizições irão passar por esse middleware
 app.use(routes);
 
 app.listen(3000, () => {
